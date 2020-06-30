8ce6f9c5def52671640aae260bd131ec4e685c30
Mico <mico@Micos-MacBook-Pro.local>
2020-06-30 11:58:05 -0400

    Added File Upload And Download

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,39 @@
 import React, { Component } from 'react';
 
 export default class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      file: {}
+    }
+
+    this.handleChange=this.handleChange.bind(this)
+    this.handleDownload=this.handleDownload.bind(this)
+  }
+  
+  handleChange(event) {
+    debugger
+    this.setState({file: event.target.files[0] })
+  }
+
+  handleDownload() {
+    const url = window.URL.createObjectURL(this.state.file)
+    const link = document.createElement("a")
+    link.download = this.state.file.name 
+    link.href = url
+    document.body.appendChild(link)
+    link.click()
+    link.remove()
+    window.URL.revokeObjectURL(url)
+
+  }
+
   render() {
     return (
       <div className='app'>
-        <h1>DevCamp React Starter</h1>
-        <h2>React Redux Router</h2>
+        <input onChange={this.handleChange} type="file"/>
+        <button onClick={this.handleDownload}>Download</button>
       </div>
     );
   }
